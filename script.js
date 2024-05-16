dbca67eff00d06649f9c6f8fbfc72f732a5d6cea
DanJenkins-Developer <djenkinsjg0617@gmail.com>
2024-05-15 22:04:27 -0500

    getting pics with index

--- script.js
+++ script.js
@@ -2,8 +2,34 @@
 
 const cardellPicEl = document.getElementById("cardell_pic")
 
+const picNames = ['20220406_132216.jpg', 'E77FBF21-9EB8-49B7-80EB-DA33BF4FD718.jpg', 'IMG_0928.jpg', '7473AC61-57B2-42E3-8C16-51D615774F1E.jpg', 'IMG_0592.jpg' , 'IMG_5148.png']
+let index = 0
+const picsNamesLength = picNames.length - 1
 
-const getRandomPic = (index)  =>  {
+
+// const getRandomPic = (index)  =>  {
+    
+//     const currentDir = window.location.pathname;
+
+//     const pathWithoutFile = currentDir.replace(/\/[^/]+$/, "");
+
+//     const picsDir = pathWithoutFile + "/cardell_pics/"
+
+//     const picNames = ['20220406_132216.jpg', 'E77FBF21-9EB8-49B7-80EB-DA33BF4FD718.jpg', 'IMG_0928.jpg', '7473AC61-57B2-42E3-8C16-51D615774F1E.jpg', 'IMG_0592.jpg' , 'IMG_5148.png']
+
+//     const min = 0;
+//     const max = 2;
+
+//     const randomNumber = Math.floor(Math.random() * (max-min + 1)) + min;
+
+//     const randomPicName = picNames[randomNumber]
+
+//     const randomPicPath = picsDir + randomPicName
+
+//     return randomPicPath
+// }
+
+const getNextPicturePath = ()  =>  {
     
     const currentDir = window.location.pathname;
 
@@ -11,16 +37,9 @@ const getRandomPic = (index)  =>  {
 
     const picsDir = pathWithoutFile + "/cardell_pics/"
 
-    const picNames = ['20220406_132216.jpg', 'E77FBF21-9EB8-49B7-80EB-DA33BF4FD718.jpg', 'IMG_0928.jpg']
+    
 
-    const min = 0;
-    const max = 2;
-
-    const randomNumber = Math.floor(Math.random() * (max-min + 1)) + min;
-
-    const randomPicName = picNames[randomNumber]
-
-    const randomPicPath = picsDir + randomPicName
+    const randomPicPath = picsDir + picNames[index]
 
     return randomPicPath
 }
@@ -35,14 +54,19 @@ const getRandomPic = (index)  =>  {
 
 const updateImage = () => {
 
-    const pic = getRandomPic();
-    //const pic = getPicture();
+    //const pic = getRandomPic();
+    const pic = getNextPicturePath()
     console.log("From function ::" + pic);
     cardellPicEl.src = pic;
     
     // Apply fade-in animation
     cardellPicEl.classList.remove("fade-out");
     cardellPicEl.classList.add("fade-in");
+
+    index = index + 1
+
+    if(index > picsNamesLength)
+        index = 0
     
     setTimeout(() => {
         // Apply fade-out animation after 3 seconds
